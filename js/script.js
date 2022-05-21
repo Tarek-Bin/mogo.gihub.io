b692d91c1c8e8a6080f6b860743079ba5dd26d3b
Tarek <tarekbinjiad7765rup@gmail.com>
2022-05-21 15:40:36 +0600

    mogo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$(function(){
+
+  $('.counter').counterUp();
+
+  $('.slider_slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:".right",
+    prevArrow:".left",
+    
+    
+  });
+
+  
+  }); 
